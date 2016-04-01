0f3dcf4132e47cc7be47815e8ea2d4c51a6581e2
Jason Ellis <palamedes@rocketmail.com>
2016-04-01 11:43:46 -0400

    Adding files

---
+++ setup.bat
@@ -0,0 +1,5 @@
+npm install babel webpack webpack-dev-server
+npm install babel-loader babel-core babel-preset-es2015 babel-preset-react
+npm install react react-dom --save
+touch index.html App.js main.js webpack.config.js package.json
+
